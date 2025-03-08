343e9b948d22846dca1ccbe15742008b7f50e33d
Ashamaz Shomakhov <ashomakhov@natera.com>
2025-03-08 22:51:33 +0100

    Task 2.1: Added User model and updated Liquibase migration

---
+++ src/main/java/ci/ashamaz/languageflash/model/User.java
@@ -0,0 +1,45 @@
+package ci.ashamaz.languageflash.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "settings")
+    private String settings;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+}
