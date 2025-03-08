b9f03c9ffde6497e1d1552400266976b6b897fa1
Ashamaz Shomakhov <ashomakhov@natera.com>
2025-03-08 22:37:37 +0100

    init

---
+++ src/main/java/ci/ashamaz/languageflash/LanguageflashApplication.java
@@ -0,0 +1,13 @@
+package ci.ashamaz.languageflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageflashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageflashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ci/ashamaz/languageflash/LanguageflashApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.ashamaz.languageflash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageflashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
